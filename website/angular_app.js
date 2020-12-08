514caf9a1b89b6adb71a0a23fa57a9c66652d231
2047214 <69069495+2047214@users.noreply.github.com>
2020-12-08 11:10:16 +0530

    NAME

--- website/angular_app.js
+++ website/angular_app.js
@@ -5,6 +5,7 @@ angular.module("MyApp",[])
         {name:'Mrs.MALINI',dob:new Date("JULY 21,1990"),gender:"FEMALE",salary:89000.988},
         {name:'Mr.YOGESH',dob:new Date("OCTOBER 02,1987"),gender:"MALE",salary:98000.740},
         {name:'Miss.PAVIKA',dob:new Date("AUGUST 14,1983"),gender:"FEMALE",salary:67540.324},
+        {name:'Miss.KARTHIKA',dob:new Date("JANUARY 20,1989"),gender:"FEMALE",salary:87540.674},
         {name:'Mr.DHILIP',dob:new Date("NOVEMBER 21,1990"),gender:"MALE",salary:78000.896},
         {name:'Mr.ADEN',dob:new Date("MAY 25,1995"),gender:"MALE",salary:76890.765},
         {name:'Miss.ZENIA FATHIMA',dob:new Date("FEBRUARY 28,1992"),gender:"FEMALE",salary:70000.00},
