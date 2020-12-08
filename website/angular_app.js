ae1cbe5e8ae2a59d738355c777b2a0885fcb76a5
2047214 <69069495+2047214@users.noreply.github.com>
2020-12-08 10:52:45 +0530

    ANGULAR

--- website/angular_app.js
+++ website/angular_app.js
@@ -2,10 +2,13 @@ angular.module("MyApp",[])
 .controller("MyCtrl",function($scope)
 {
     var employees=[
-        {name:'Rithul',dob:new Date("July 21,1990"),gender:"Male",salary:55000.778},
-        {name:'Indu',dob:new Date("July 20,1987"),gender:"Female",salary:65000.778},
-        {name:'Kevin',dob:new Date("November 19,1983"),gender:"Male",salary:55000.778},
-        {name:'Reena',dob:new Date("August 21,1990"),gender:"Male",salary:55000.778}
+        {name:'Mrs.MALINI',dob:new Date("JULY 21,1990"),gender:"FEMALE",salary:89000.988},
+        {name:'Mr.YOGESH',dob:new Date("OCTOBER 02,1987"),gender:"MALE",salary:98000.740},
+        {name:'Miss.PAVIKA',dob:new Date("AUGUST 14,1983"),gender:"FEMALE",salary:67540.324},
+        {name:'Mr.DHILIP',dob:new Date("NOVEMBER 21,1990"),gender:"MALE",salary:78000.896},
+        {name:'Mr.ADEN',dob:new Date("MAY 25,1995"),gender:"MALE",salary:76890.765},
+        {name:'Miss.ZENIA FATHIMA',dob:new Date("FEBRUARY 28,1992"),gender:"FEMALE",salary:70000.00},
+        {name:'Mr.PRASHANTH YADAV',dob:new Date("SEPTEMBER 12,1993"),gender:"MALE",salary:98767.080}
     ];
     console.log(employees);
     $scope.employees=employees;
